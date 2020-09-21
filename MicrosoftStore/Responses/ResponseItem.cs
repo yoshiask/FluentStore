d3a3d2cda9db33b1298c3c5efe536c3577373700
Joshua Askharoun <jjask7@gmail.com>
2020-09-21 15:56:32 -0500

    Added generic ResponseItem class

--- MicrosoftStore/Responses/ResponseItem.cs
+++ MicrosoftStore/Responses/ResponseItem.cs
@@ -10,5 +10,27 @@ namespace MicrosoftStore.Responses
         public string Path { get; set; }
         public DateTimeOffset ExpiryUtc { get; set; }
         public object Payload { get; set; }
+
+        public ResponseItem<TPayload> Convert<TPayload>()
+		{
+            return new ResponseItem<TPayload>(this);
+		}
+    }
+
+    public class ResponseItem<TPayload>
+    {
+        [JsonProperty(PropertyName = "$type")]
+        public string TypeName { get; set; }
+        public string Path { get; set; }
+        public DateTimeOffset ExpiryUtc { get; set; }
+        public TPayload Payload { get; set; }
+
+        public ResponseItem(ResponseItem item)
+		{
+            TypeName = item.TypeName;
+            Path = item.Path;
+            ExpiryUtc = item.ExpiryUtc;
+            Payload = (item.Payload as Newtonsoft.Json.Linq.JObject).ToObject<TPayload>();
+        }
     }
 }
