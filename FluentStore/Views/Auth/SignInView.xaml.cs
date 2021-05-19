5dae4ad9a4c7e9e4e9a776eced4075baafc520fe
Yoshi Askharoun <jjask7@gmail.com>
2021-05-19 13:09:51 -0500

    Added Enter key to PasswordBox in sign in

--- FluentStore/Views/Auth/SignInView.xaml.cs
+++ FluentStore/Views/Auth/SignInView.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
+﻿using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +13,15 @@ namespace FluentStore.Views.Auth
         public SignInView()
         {
             this.InitializeComponent();
+        }
+
+        private void PasswordBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                // Attempt to sign the user in
+                ViewModel.SignInCommand.Execute(null);
+            }
         }
     }
 }
