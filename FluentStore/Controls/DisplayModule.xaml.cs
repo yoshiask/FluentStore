e4cf2e043b5c5bdab8d75f8f0be009d976a586f0
Yoshi Askharoun <jjask7@gmail.com>
2021-08-08 23:07:48 -0500

    Added DisplayInfo control

---
+++ FluentStore/Controls/DisplayModule.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace FluentStore.Controls
+{
+    public sealed partial class DisplayModule : UserControl
+    {
+        public DisplayModule()
+        {
+            this.InitializeComponent();
+        }
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(
+            nameof(Title), typeof(string), typeof(DisplayModule), new PropertyMetadata(string.Empty));
+
+        public object Value
+        {
+            get => GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            nameof(Value), typeof(object), typeof(DisplayModule), new PropertyMetadata(null));
+    }
+}
