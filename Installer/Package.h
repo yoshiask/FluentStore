2922efa94c67af302a56f9569639f6c8111d547a
Yoshi Askharoun <jjask7@gmail.com>
2021-08-20 18:07:14 -0500

    Compiled 0.1.0.0 installer

--- Installer/Package.h
+++ Installer/Package.h
@@ -4,7 +4,7 @@
 #define CERTIFICATE_PUBLISHER L"CN=CD3113F3-18EB-4B8D-9D8A-C6D373E7A464"
 
 // ID used to launch your app ("PackageFamilyName!App")
-#define APP_USER_MODEL_ID L"52374YoshiAskharoun.FluentStore!App"
+#define APP_USER_MODEL_ID L"52374YoshiAskharoun.FluentStore_bcem08bwhrc72!App"
 
 // Header file with text to use
 #include "Text en-US.h"
