4ec612349e66970c0eb0e4febc36f9a9c4d3c784
Yoshi Askharoun <jjask7@gmail.com>
2021-08-26 00:00:50 -0500

    Bumped version to 0.1.2

--- Installer/App.xaml.cs
+++ Installer/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Installer
     {
         public static MainWindow InstallerWindow;
 
-        public static Version Version { get; } = new Version(0, 1, 1, 0);
+        public static Version Version { get; } = new Version(0, 1, 2, 0);
         public static string VersionString => Version.ToString();
 
         public App()
