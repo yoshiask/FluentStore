964cbdf81dec510c35bc7af77116b257d45ffdac
Yoshi Askharoun <jjask7@gmail.com>
2021-09-08 00:04:32 -0500

    Added UWPC package handler

---
+++ FluentStore.SDK/Handlers/UwpCommunityHandler.cs
@@ -0,0 +1,142 @@
+﻿using FluentStore.SDK.Images;
+using FluentStore.SDK.Packages;
+using Flurl;
+using Flurl.Http;
+using Garfoot.Utilities.FluentUrn;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using FSAPI = FluentStoreAPI.FluentStoreAPI;
+
+namespace FluentStore.SDK.Handlers
+{
+    public class UwpCommunityHandler : PackageHandlerBase
+    {
+        // TODO: Switch web requests to official UWPC library when available
+
+        private readonly PackageService PackageService = Ioc.Default.GetRequiredService<PackageService>();
+        private const string BASE_URL = "https://uwpcommunity-site-backend.herokuapp.com";
+
+        public const string NAMESPACE_PROJECT = "uwpc-projects";
+        public override HashSet<string> HandledNamespaces => new HashSet<string>
+        {
+            NAMESPACE_PROJECT,
+        };
+
+        public override string DisplayName => "UWP Community";
+
+        public override async Task<List<PackageBase>> GetFeaturedPackagesAsync()
+        {
+            var now = DateTime.UtcNow;
+            int launchYear = now.Year;
+            if (now.Month < 9)
+                launchYear--;
+            var projects = (await BASE_URL.AppendPathSegments("projects", "launch", launchYear.ToString())
+                .GetJsonAsync()).projects;
+
+            var packages = new List<PackageBase>(projects.Count);
+            foreach (dynamic project in projects)
+            {
+                var package = new UwpCommunityPackage(project)
+                {
+                    Status = PackageStatus.BasicDetails
+                };
+                packages.Add(package);
+            }
+
+            return packages;
+        }
+
+        public override async Task<PackageBase> GetPackage(Urn urn)
+        {
+            if (urn.NamespaceIdentifier == NAMESPACE_PROJECT)
+                return await GetPackage(urn.GetContent<NamespaceSpecificString>().UnEscapedValue);
+
+            return null;
+        }
+
+        public async Task<PackageBase> GetPackage(string projectIdStr)
+        {
+            int projectId = int.Parse(projectIdStr);
+
+            var projects = await BASE_URL.AppendPathSegments("projects").GetJsonListAsync();
+            dynamic project = projects.FirstOrDefault(p => p.id == projectId);
+            if (project == null)
+            {
+                var NavService = Ioc.Default.GetRequiredService<Services.INavigationService>();
+                NavService.ShowHttpErrorPage(404, "That project is not registered with the UWP Community.");
+                return null;
+            }
+
+            var package = new UwpCommunityPackage(project);
+
+            var images = await BASE_URL.AppendPathSegments("projects", "images")
+                .SetQueryParam("projectId", projectIdStr).GetJsonAsync<List<string>>();
+            package.UpdateWithImages(images);
+
+            var collaborators = await BASE_URL.AppendPathSegments("projects", "collaborators")
+                .SetQueryParam("projectId", projectIdStr).GetJsonAsync<List<dynamic>>();
+            package.UpdateWithCollaborators(collaborators);
+
+            package.Status = PackageStatus.DownloadReady;
+            return package;
+        }
+
+        public override async Task<List<PackageBase>> GetSearchSuggestionsAsync(string query)
+        {
+            // TODO
+            return new List<PackageBase>(0);
+        }
+
+        public override async Task<List<PackageBase>> SearchAsync(string query)
+        {
+            // TODO
+            return new List<PackageBase>(0);
+        }
+
+        public override ImageBase GetImage() => GetImageStatic();
+        public static ImageBase GetImageStatic()
+        {
+            return new FileImage
+            {
+                Url = "https://github.com/UWPCommunity/UWPCommunityApp/blob/dev/LaunchAssets/2020/AppIcon.png?raw=true"
+            };
+        }
+
+        public override async Task<PackageBase> GetPackageFromUrl(Url url)
+        {
+            if (url.Host.EndsWith("uwpcommunity.com"))
+            {
+                if (url.QueryParams.TryGetFirst("project", out object projectId))
+                {
+                    return await GetPackage(projectId.ToString());
+                }
+
+                // TODO: This code is here just in case the UWPC website gets more complicated.
+                //Regex launchRx = new Regex(@"\/launch\/(?<launchYear>\d+)\/?\?project=(?<projId>\d+)");
+                //Match launchMc = launchRx.Match(url);
+                //if (launchMc.Success)
+                //{
+                //    return await GetPackage(launchMc.Groups["projId"].Value);
+                //}
+
+                //Regex projectsRx = new Regex(@"\/projects\/?(\?project=)?(?<projId>\d+)");
+                //Match projectsMc = projectsRx.Match(url);
+                //if (projectsMc.Success)
+                //{
+                //    return await GetPackage(launchMc.Groups["projId"].Value);
+                //}
+            }
+
+            return null;
+        }
+
+        public override Url GetUrlFromPackage(PackageBase package)
+        {
+            return "fluentstore://package/" + package.Urn.ToString();
+        }
+    }
+}
---
+++ FluentStore.SDK/PackageTypes/UwpCommunityPackage.cs
@@ -0,0 +1,162 @@
+﻿using FluentStore.SDK.Images;
+using FluentStore.Services;
+using Flurl;
+using Garfoot.Utilities.FluentUrn;
+using Microsoft.Toolkit.Diagnostics;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace FluentStore.SDK.Packages
+{
+    public class UwpCommunityPackage : PackageBase<dynamic>
+    {
+        INavigationService NavigationService = Ioc.Default.GetRequiredService<INavigationService>();
+        PackageService PackageService = Ioc.Default.GetRequiredService<PackageService>();
+
+        public UwpCommunityPackage(dynamic project = null, IEnumerable<string> images = null, IEnumerable<dynamic> collaborators = null)
+        {
+            if (project != null)
+                UpdateWithProject(project);
+            if (images != null)
+                UpdateWithImages(images);
+            if (collaborators != null)
+                UpdateWithCollaborators(collaborators);
+        }
+
+        public void UpdateWithProject(dynamic project)
+        {
+            Guard.IsNotNull(project, nameof(project));
+            Model = project;
+
+            // Set base properties
+            Title = project.appName;
+            Description = project.description;
+            ReleaseDate = project.createdAt;
+
+            if (project.heroImage != null)
+                Images.Add(new FileImage
+                {
+                    Url = project.heroImage,
+                    ImageType = ImageType.Hero
+                });
+
+            if (project.appIcon != null)
+                Images.Add(new FileImage
+                {
+                    Url = project.appIcon,
+                    ImageType = ImageType.Logo,
+                    BackgroundColor = project.accentColor,
+                });
+            else
+                Images.Add(TextImage.CreateFromName(Title));
+
+            // Set UWPC properties
+            ProjectId = (int)project.id;
+            DownloadLink = project.downloadLink;
+        }
+
+        public void UpdateWithImages(IEnumerable<string> images)
+        {
+            Guard.IsNotNull(images, nameof(images));
+
+            foreach (string img in images)
+            {
+                Images.Add(new FileImage
+                {
+                    Url = img,
+                    ImageType = ImageType.Screenshot
+                });
+            }
+        }
+
+        public void UpdateWithCollaborators(IEnumerable<dynamic> collaborators)
+        {
+            Guard.IsNotNull(collaborators, nameof(collaborators));
+
+            dynamic owner = collaborators.FirstOrDefault(c => c.isOwner == true);
+            if (owner != null)
+                DeveloperName = owner.name;
+        }
+
+        private Urn _Urn;
+        public override Urn Urn
+        {
+            get
+            {
+                if (_Urn == null)
+                    _Urn = Urn.Parse("urn:" + Handlers.UwpCommunityHandler.NAMESPACE_PROJECT + ":" + ProjectId);
+                return _Urn;
+            }
+            set => _Urn = value;
+        }
+
+        public override Task<bool> CanLaunchAsync() => Task.FromResult(false);
+
+        public override async Task<IStorageItem> DownloadPackageAsync(StorageFolder folder = null)
+        {
+            LinkedPackage = await PackageService.GetPackageFromUrlAsync(DownloadLink);
+            if (LinkedPackage != null)
+            {
+                return await LinkedPackage.DownloadPackageAsync(folder);
+            }
+            else
+            {
+                Status = await NavigationService.OpenInBrowser(DownloadLink)
+                    ? PackageStatus.Downloaded : PackageStatus.DownloadReady;
+                return null;
+            }
+        }
+
+        public override async Task<ImageBase> GetAppIcon()
+        {
+            return Images.FirstOrDefault(i => i.ImageType == ImageType.Logo)
+                ?? TextImage.CreateFromName(Title);
+        }
+
+        public override async Task<ImageBase> GetHeroImage()
+        {
+            return Images.FirstOrDefault(i => i.ImageType == ImageType.Hero);
+        }
+
+        public override async Task<List<ImageBase>> GetScreenshots()
+        {
+            return Images.Where(i => i.ImageType == ImageType.Screenshot).ToList();
+        }
+
+        public override async Task<bool> InstallAsync()
+        {
+            if (LinkedPackage != null)
+                return await LinkedPackage.InstallAsync();
+            else
+                return true;
+        }
+
+        public override Task LaunchAsync() => throw new NotImplementedException();
+
+        private int _ProjectId;
+        public int ProjectId
+        {
+            get => _ProjectId;
+            set => SetProperty(ref _ProjectId, value);
+        }
+
+        private Url _DownloadLink;
+        public Url DownloadLink
+        {
+            get => _DownloadLink;
+            set => SetProperty(ref _DownloadLink, value);
+        }
+
+        private PackageBase _LinkedPackage;
+        public PackageBase LinkedPackage
+        {
+            get => _LinkedPackage;
+            set => SetProperty(ref _LinkedPackage, value);
+        }
+    }
+}
