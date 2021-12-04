e17a64e26d04e06b60af289d53897a2f5f007f63
Yoshi Askharoun <jjask7@gmail.com>
2021-12-03 23:27:40 -0600

    Added dark title bar

--- FluentStore.App/MainWindow.xaml.cs
+++ FluentStore.App/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.Messaging;
 using Microsoft.UI.Xaml;
 using System;
+using System.Runtime.InteropServices;
+using PInvoke;
+using HRESULT = Vanara.PInvoke.HRESULT;
+using DwmApi = Vanara.PInvoke.DwmApi;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -13,14 +17,18 @@ namespace FluentStore
     public sealed partial class MainWindow : Window
     {
         private IntPtr m_hwnd;
+        private WinProc newWndProc = null;
+        private IntPtr oldWndProc = IntPtr.Zero;
 
         public MainWindow()
         {
             this.InitializeComponent();
 
             m_hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            SubClassing();
 
             LoadIcon(@"Assets\AppIcon.ico");
+            UpdateTitleBarTheme();
 
             WeakReferenceMessenger.Default.Register<ViewModels.Messages.SetPageHeaderMessage>(this, (r, m) =>
             {
@@ -30,20 +38,71 @@ namespace FluentStore
             });
         }
 
+        [DllImport("user32")]
+        private static extern IntPtr SetWindowLongPtr(IntPtr hWnd, User32.WindowLongIndexFlags nIndex, WinProc newProc);
+        [DllImport("user32")]
+        static extern IntPtr CallWindowProc(IntPtr lpPrevWndFunc, IntPtr hWnd, User32.WindowMessage Msg, IntPtr wParam, IntPtr lParam);
+        enum BOOL
+        {
+            FALSE = 0,
+            TRUE = 1
+        };
+
+        private delegate IntPtr WinProc(IntPtr hWnd, User32.WindowMessage Msg, IntPtr wParam, IntPtr lParam);
+
+        private void SubClassing()
+        {
+            newWndProc = new WinProc(NewWindowProc);
+            Kernel32.SetLastError(0);
+            oldWndProc = SetWindowLongPtr(m_hwnd, User32.WindowLongIndexFlags.GWL_WNDPROC, newWndProc);
+            var hr = Kernel32.GetLastError();
+            new HRESULT(-(int)hr).ThrowIfFailed();
+        }
+
         private void LoadIcon(string iconName)
         {
-            IntPtr hIcon = PInvoke.User32.LoadImage(IntPtr.Zero, iconName,
-                PInvoke.User32.ImageType.IMAGE_ICON, 16, 16, PInvoke.User32.LoadImageFlags.LR_LOADFROMFILE);
+            IntPtr hIcon = User32.LoadImage(IntPtr.Zero, iconName,
+                User32.ImageType.IMAGE_ICON, 16, 16, User32.LoadImageFlags.LR_LOADFROMFILE);
 
-            var hresult = PInvoke.Kernel32.GetLastError();
-            if (hresult != PInvoke.Win32ErrorCode.ERROR_SUCCESS)
-                throw new PInvoke.Win32Exception(hresult);
+            var hresult = Kernel32.GetLastError();
+            if (hresult != Win32ErrorCode.ERROR_SUCCESS)
+                throw new Win32Exception(hresult);
 
-            PInvoke.User32.SendMessage(m_hwnd, PInvoke.User32.WindowMessage.WM_SETICON, IntPtr.Zero, hIcon);
+            User32.SendMessage(m_hwnd, User32.WindowMessage.WM_SETICON, IntPtr.Zero, hIcon);
 
-            hresult = PInvoke.Kernel32.GetLastError();
-            if (hresult != PInvoke.Win32ErrorCode.ERROR_SUCCESS)
-                throw new PInvoke.Win32Exception(hresult);
+            hresult = Kernel32.GetLastError();
+            if (hresult != Win32ErrorCode.ERROR_SUCCESS)
+                throw new Win32Exception(hresult);
+        }
+
+        private unsafe IntPtr NewWindowProc(IntPtr hWnd, User32.WindowMessage Msg, IntPtr wParam, IntPtr lParam)
+        {
+            switch (Msg)
+            {
+                case User32.WindowMessage.WM_SETTINGCHANGE:
+                case User32.WindowMessage.WM_THEMECHANGED:
+                    HRESULT hr = UpdateTitleBarTheme();
+                    hr.ThrowIfFailed();
+                    break;
+
+                case User32.WindowMessage.WM_GETMINMAXINFO:
+                    var dpi = User32.GetDpiForWindow(hWnd);
+                    float scalingFactor = (float)dpi / 96;
+
+                    //MINMAXINFO minMaxInfo = Marshal.PtrToStructure<MINMAXINFO>(lParam);
+                    //minMaxInfo.ptMinTrackSize.x = (int)(MinWidth * scalingFactor);
+                    //minMaxInfo.ptMinTrackSize.y = (int)(MinHeight * scalingFactor); 
+                    //Marshal.StructureToPtr(minMaxInfo, lParam, true);
+                    break;
+                    
+            }
+            return CallWindowProc(oldWndProc, hWnd, Msg, wParam, lParam);
+        }
+
+        private unsafe HRESULT UpdateTitleBarTheme()
+        {
+            BOOL isDark = MainContent.ActualTheme == ElementTheme.Dark ? BOOL.TRUE : BOOL.FALSE;
+            return DwmApi.DwmSetWindowAttribute(m_hwnd, (DwmApi.DWMWINDOWATTRIBUTE)20, new(&isDark), sizeof(BOOL));
         }
     }
 }
