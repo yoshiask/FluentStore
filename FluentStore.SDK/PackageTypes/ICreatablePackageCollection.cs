4beb91239e967c870de7c4f43062654b6398ddfe
Yoshi Askharoun <jjask7@gmail.com>
2022-03-24 21:53:59 -0500

    Added interfaces for creating packages

---
+++ FluentStore.SDK/PackageTypes/ICreatablePackageCollection.cs
@@ -0,0 +1,9 @@
+﻿namespace FluentStore.SDK.Packages
+{
+    /// <summary>
+    /// Represents a collection that can be created, whose details and items are editable.
+    /// </summary>
+    public interface ICreatablePackageCollection : IPackageCollection, IEditablePackage, IEditablePackageCollection
+    {
+    }
+}
--- FluentStore.SDK/PackageTypes/IPackageCollection.cs
+++ FluentStore.SDK/PackageTypes/IPackageCollection.cs
@@ -2,8 +2,14 @@
 
 namespace FluentStore.SDK.Packages
 {
+    /// <summary>
+    /// Represents a collection.
+    /// </summary>
     public interface IPackageCollection
     {
+        /// <summary>
+        /// The list of packages in this collection.
+        /// </summary>
         public ObservableCollection<PackageBase> Items { get; set; }
     }
 }
