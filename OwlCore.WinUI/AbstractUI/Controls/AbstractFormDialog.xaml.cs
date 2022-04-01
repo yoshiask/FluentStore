f2a3b96cd11f4796f8835023ce14ba9a7bdb782f
Yoshi Askharoun <jjask7@gmail.com>
2022-04-01 18:35:48 -0500

    Added AbstractFormDialog

---
+++ OwlCore.WinUI/AbstractUI/Controls/AbstractFormDialog.xaml.cs
@@ -0,0 +1,51 @@
+﻿using FluentStore.SDK.AbstractUI.Models;
+using FluentStore.SDK.AbstractUI.ViewModels;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace OwlCore.WinUI.AbstractUI.Controls
+{
+    public sealed partial class AbstractFormDialog : ContentDialog
+    {
+        /// <summary>
+        /// Backing property for <see cref="ViewModel"/>.
+        /// </summary>
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register(nameof(ViewModel), typeof(AbstractFormViewModel), typeof(AbstractFormDialog), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Backing property for <see cref="TemplateSelector"/>.
+        /// </summary>
+        public static readonly DependencyProperty TemplateSelectorProperty =
+            DependencyProperty.Register(nameof(TemplateSelector), typeof(DataTemplateSelector), typeof(AbstractFormDialog), new PropertyMetadata(null));
+
+        /// <summary>
+        /// The ViewModel for this UserControl.
+        /// </summary>
+        public AbstractFormViewModel? ViewModel
+        {
+            get => (AbstractFormViewModel)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
+
+        /// <summary>
+        /// The template selector used to display Abstract UI elements. Use this to define your own custom styles for each control. You may specify the existing, default styles for those you don't want to override.
+        /// </summary>
+        public DataTemplateSelector? TemplateSelector
+        {
+            get => (DataTemplateSelector)GetValue(TemplateSelectorProperty);
+            set => SetValue(TemplateSelectorProperty, value);
+        }
+
+        public AbstractFormDialog(XamlRoot root)
+        {
+            InitializeComponent();
+            XamlRoot = root;
+        }
+
+        public AbstractFormDialog(AbstractForm form, XamlRoot root) : this(root)
+        {
+            ViewModel = new(form);
+        }
+    }
+}
