e6d26af65237e2c0fc2f9687b0bde8608dfa4849
Yoshi Askharoun <jjask7@gmail.com>
2022-06-16 13:37:45 -0500

    Moved title bar to MainWindow, mica tweaks

--- FluentStore.App/MainWindow.xaml.cs
+++ FluentStore.App/MainWindow.xaml.cs
@@ -3,10 +3,9 @@ using FluentStore.ViewModels.Messages;
 using Microsoft.UI.Xaml;
 using System;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using PInvoke;
 using HRESULT = Vanara.PInvoke.HRESULT;
 using DwmApi = Vanara.PInvoke.DwmApi;
+using WinUIEx;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -16,11 +15,9 @@ namespace FluentStore
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : Window, IRecipient<SetPageHeaderMessage>
+    public sealed partial class MainWindow : WindowEx, IRecipient<SetPageHeaderMessage>
     {
         private IntPtr m_hwnd;
-        private WinProc newWndProc = null;
-        private IntPtr oldWndProc = IntPtr.Zero;
         private Stack<object> m_navStack = new();
 
         public MainWindow()
@@ -28,10 +25,19 @@ namespace FluentStore
             this.InitializeComponent();
 
             m_hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
-            SubClassing();
 
-            LoadIcon(@"Assets\AppIcon.ico");
-            UpdateTitleBarTheme();
+            TaskBarIcon = Icon.FromFile(@"Assets\AppIcon.ico");
+
+            WeakReferenceMessenger.Default.Register(this);
+
+            var titleBar = AppWindow?.TitleBar;
+            if (titleBar != null)
+            {
+                titleBar.ExtendsContentIntoTitleBar = true;
+                titleBar.ButtonBackgroundColor = Microsoft.UI.Colors.Transparent;
+                titleBar.ButtonInactiveBackgroundColor = Microsoft.UI.ColorHelper.FromArgb(10, 0, 0, 0);
+                SetTitleBar(CustomTitleBar);
+            }
         }
 
         public IntPtr Handle => m_hwnd;
@@ -77,70 +83,17 @@ namespace FluentStore
 
         public void ClearNavigationStack() => m_navStack.Clear();
 
-        [DllImport("user32")]
-        private static extern IntPtr SetWindowLongPtr(IntPtr hWnd, User32.WindowLongIndexFlags nIndex, WinProc newProc);
-        [DllImport("user32")]
-        static extern IntPtr CallWindowProc(IntPtr lpPrevWndFunc, IntPtr hWnd, User32.WindowMessage Msg, IntPtr wParam, IntPtr lParam);
+        private unsafe HRESULT EnableThemeAwareTitleBar()
+        {
+            BOOL useDark = BOOL.TRUE;
+            return DwmApi.DwmSetWindowAttribute(m_hwnd, (DwmApi.DWMWINDOWATTRIBUTE)20, new(&useDark), sizeof(BOOL));
+        }
+
         enum BOOL
         {
             FALSE = 0,
             TRUE = 1
         };
-
-        private delegate IntPtr WinProc(IntPtr hWnd, User32.WindowMessage Msg, IntPtr wParam, IntPtr lParam);
-
-        private void SubClassing()
-        {
-            newWndProc = new WinProc(NewWindowProc);
-            Kernel32.SetLastError(0);
-            oldWndProc = SetWindowLongPtr(m_hwnd, User32.WindowLongIndexFlags.GWL_WNDPROC, newWndProc);
-            Marshal.ThrowExceptionForHR(Marshal.GetLastWin32Error());
-        }
-
-        private void LoadIcon(string iconName)
-        {
-            IntPtr hIcon = User32.LoadImage(IntPtr.Zero, iconName,
-                User32.ImageType.IMAGE_ICON, 16, 16, User32.LoadImageFlags.LR_LOADFROMFILE);
-            Marshal.ThrowExceptionForHR(Marshal.GetLastWin32Error());
-
-            User32.SendMessage(m_hwnd, User32.WindowMessage.WM_SETICON, IntPtr.Zero, hIcon);
-            Marshal.ThrowExceptionForHR(Marshal.GetLastWin32Error());
-        }
-
-        private unsafe IntPtr NewWindowProc(IntPtr hWnd, User32.WindowMessage Msg, IntPtr wParam, IntPtr lParam)
-        {
-            switch (Msg)
-            {
-                case User32.WindowMessage.WM_SETTINGCHANGE:
-                case User32.WindowMessage.WM_THEMECHANGED:
-                    HRESULT hr = UpdateTitleBarTheme();
-                    hr.ThrowIfFailed();
-                    break;
-
-                case User32.WindowMessage.WM_GETMINMAXINFO:
-                    var dpi = User32.GetDpiForWindow(hWnd);
-                    float scalingFactor = (float)dpi / 96;
-
-                    //MINMAXINFO minMaxInfo = Marshal.PtrToStructure<MINMAXINFO>(lParam);
-                    //minMaxInfo.ptMinTrackSize.x = (int)(MinWidth * scalingFactor);
-                    //minMaxInfo.ptMinTrackSize.y = (int)(MinHeight * scalingFactor); 
-                    //Marshal.StructureToPtr(minMaxInfo, lParam, true);
-                    break;
-            }
-            return CallWindowProc(oldWndProc, hWnd, Msg, wParam, lParam);
-        }
-
-        private unsafe HRESULT UpdateTitleBarTheme()
-        {
-            bool isDark;
-            if (Content is FrameworkElement element)
-                isDark = element.ActualTheme == ElementTheme.Dark;
-            else
-                isDark = App.Current.RequestedTheme == ApplicationTheme.Dark;
-
-            BOOL winIsDark = isDark ? BOOL.TRUE : BOOL.FALSE;
-            return DwmApi.DwmSetWindowAttribute(m_hwnd, (DwmApi.DWMWINDOWATTRIBUTE)20, new(&winIsDark), sizeof(BOOL));
-        }
 
         void IRecipient<SetPageHeaderMessage>.Receive(SetPageHeaderMessage m)
         {
