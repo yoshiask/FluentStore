7548363a9593be2743e23bfcf6bf349e9bc3136d
Yoshi Askharoun <jjask7@gmail.com>
2023-12-05 12:26:09 -0600

    Add Firebase tests

---
+++ Tests/SDKTests/Firebase.cs
@@ -0,0 +1,16 @@
+﻿namespace SDKTests;
+
+public class Firebase
+{
+    [Fact]
+    public async Task ListDefaultPlugins()
+    {
+        FluentStoreAPI.FluentStoreAPI api = new();
+
+        var pluginDefaults = await api.GetPluginDefaultsAsync();
+
+        Assert.NotNull(pluginDefaults);
+        Assert.NotEmpty(pluginDefaults.Packages);
+        Assert.NotEmpty(pluginDefaults.Feeds);
+    }
+}
