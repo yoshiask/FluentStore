4513f671bd2e79efeb5f110f7728d8f92c2ccd0d
Yoshi Askharoun <jjask7@gmail.com>
2024-08-27 14:31:58 -0500

    Added special PluginPackageBase

--- FluentStore.SDK/Plugins/Sources/NuGetPluginPackage.cs
+++ FluentStore.SDK/Plugins/Sources/NuGetPluginPackage.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Messaging;
 using FluentStore.SDK.Downloads;
 using FluentStore.SDK.Helpers;
 using FluentStore.SDK.Images;
@@ -6,26 +7,21 @@ using Garfoot.Utilities.FluentUrn;
 using NuGet.Packaging;
 using NuGet.Protocol.Core.Types;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
 namespace FluentStore.SDK.Plugins.Sources;
 
-public partial class NuGetPluginPackage : PackageBase
+public partial class NuGetPluginPackage : PluginPackageBase
 {
-    private readonly PluginLoader _pluginLoader;
-
     private Uri _iconUri = new("https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/NuGet_project_logo.svg/240px-NuGet_project_logo.svg.png");
     private DownloadResourceResult _downloadItem;
 
     [ObservableProperty]
     private string _nuGetId;
 
-    public NuGetPluginPackage(NuGetPluginHandler packageHandler, IPackageSearchMetadata searchMetadata = null, NuspecReader nuspec = null, PackageReaderBase reader = null) : base(packageHandler)
+    public NuGetPluginPackage(NuGetPluginHandler packageHandler, IPackageSearchMetadata searchMetadata = null, NuspecReader nuspec = null, PackageReaderBase reader = null) : base(packageHandler, packageHandler.PluginLoader)
     {
-        _pluginLoader = packageHandler.PluginLoader;
-
         if (searchMetadata is not null)
             Update(searchMetadata);
         if (nuspec is not null)
@@ -36,12 +32,6 @@ public partial class NuGetPluginPackage : PackageBase
     }
 
     public override async Task<ImageBase> CacheAppIcon() => _iconUri is null ? null : new FileImage(_iconUri);
-
-    public override Task<ImageBase> CacheHeroImage() => Task.FromResult<ImageBase>(null);
-
-    public override Task<List<ImageBase>> CacheScreenshots() => Task.FromResult<List<ImageBase>>([]);
-
-    public override Task<bool> CanLaunchAsync() => Task.FromResult(false);
 
     public override async Task<FileSystemInfo> DownloadAsync(DirectoryInfo folder = null)
     {
@@ -77,11 +67,6 @@ public partial class NuGetPluginPackage : PackageBase
         }
 
         return false;
-    }
-
-    public override Task LaunchAsync()
-    {
-        throw new NotImplementedException();
     }
 
     public void Update(IPackageSearchMetadata searchMetadata)
---
+++ FluentStore.SDK/Plugins/Sources/PluginPackageBase.cs
@@ -0,0 +1,45 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using FluentStore.SDK.Images;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FluentStore.SDK.Plugins.Sources;
+
+public abstract partial class PluginPackageBase(PackageHandlerBase packageHandler, PluginLoader pluginLoader) : PackageBase(packageHandler)
+{
+    protected readonly PluginLoader _pluginLoader = pluginLoader;
+
+    [ObservableProperty]
+    private global::NuGet.Versioning.NuGetVersion _currentVersion;
+
+    [ObservableProperty]
+    private global::NuGet.Versioning.NuGetVersion _latestStableVersion;
+
+    [ObservableProperty]
+    private global::NuGet.Versioning.NuGetVersion _latestPrereleaseVersion;
+
+    public override Task<ImageBase> CacheAppIcon() => Task.FromResult<ImageBase>(TextImage.CreateFromName(ShortTitle ?? Title));
+
+    public override Task<ImageBase> CacheHeroImage() => Task.FromResult<ImageBase>(null);
+
+    public override Task<List<ImageBase>> CacheScreenshots() => Task.FromResult<List<ImageBase>>([]);
+
+    public override Task<bool> CanLaunchAsync() => Task.FromResult(false);
+
+    public override Task LaunchAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual async Task<bool> IsUpdateAvailable(bool includePrerelease = false)
+    {
+        // There isn't anything installed to update
+        if (CurrentVersion is null)
+            return false;
+
+        return
+            (includePrerelease && LatestPrereleaseVersion is not null && LatestPrereleaseVersion > CurrentVersion)
+            || (LatestStableVersion > CurrentVersion);
+    }
+}
