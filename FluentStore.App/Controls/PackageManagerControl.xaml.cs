bd8a5b65ffd57fcad2f11382a8a319051f2d7e73
Yoshi Askharoun <jjask7@gmail.com>
2024-08-27 14:29:43 -0500

    Cleanup PackageManagerControl code-behind

--- FluentStore.App/Controls/PackageManagerControl.xaml.cs
+++ FluentStore.App/Controls/PackageManagerControl.xaml.cs
@@ -1,18 +1,6 @@
 using FluentStore.ViewModels;
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 namespace FluentStore.Controls;
 
@@ -32,7 +20,6 @@ public sealed partial class PackageManagerControl : UserControl
 
         foreach (var package in e.AddedItems.Cast<PackageViewModel>())
             ViewModel.SelectedPackages.Add(package);
-
     }
 
     private void PackageListView_ItemClick(object sender, ItemClickEventArgs e)
