18a59604dd9a67773e7372d959ba5da08513e13b
Yoshi Askharoun <jjask7@gmail.com>
2025-01-09 20:38:21 -0800

    View plugin details without selecting it

--- FluentStore.App/Controls/PackagePicker.xaml.cs
+++ FluentStore.App/Controls/PackagePicker.xaml.cs
@@ -1,23 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System.Collections.ObjectModel;
 using FluentStore.SDK.Plugins.Sources;
-using FluentStore.SDK;
 using FluentStore.ViewModels;
-using OwlCore.ComponentModel;
-using System.Collections.Specialized;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -55,16 +39,25 @@ namespace FluentStore.Controls
         public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(
             nameof(ViewModel), typeof(PackageManagerViewModel), typeof(PackagePicker), new PropertyMetadata(null));
 
-        private void PackageListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            foreach (var package in e.RemovedItems.Cast<PluginPackageBase>())
-                ViewModel.SelectedPackages.Remove(package);
-
-            foreach (var package in e.AddedItems.Cast<PluginPackageBase>())
-                ViewModel.SelectedPackages.Add(package);
-        }
-
         private async void PackageListView_ItemClick(object sender, ItemClickEventArgs e) =>
             await ViewModel.ViewCommand.ExecuteAsync(e.ClickedItem as PluginPackageBase);
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            var package = (sender as FrameworkElement)?.DataContext as PluginPackageBase;
+            if (package is null)
+                return;
+
+            ViewModel.SelectedPackages.Add(package);
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            var package = (sender as FrameworkElement)?.DataContext as PluginPackageBase;
+            if (package is null)
+                return;
+
+            ViewModel.SelectedPackages.Remove(package);
+        }
     }
 }
