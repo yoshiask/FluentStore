0a5f7a83f979c5db504ba5137d3a47ff3ec00cc8
Joshua "Yoshi" Askharoun <jjask7@gmail.com>
2025-03-01 12:53:10 -0600

    Add support for x86 on ARM64

--- FluentStore.SDK/Models/Architecture.cs
+++ FluentStore.SDK/Models/Architecture.cs
@@ -4,12 +4,14 @@
     /// Represents a CPU architecture.
     /// </summary>
     /// <remarks>
-    /// The lowest 3 bits represent bitness. The upper 4 bits represent the ISA "family".
+    /// The lowest 3 bits represent bitness.
+    /// Bits 4 through 7 represent the target ISA family.
+    /// Bits 8 through 15 represent the emulated ISA family.
     /// </remarks>
-    public enum Architecture : byte
+    public enum Architecture : uint
     {
         Unknown = 0,
-        Neutral = 0xF0, // Arm | Intel,
+        Neutral = 0x00F0, // Arm | Intel,
 
         // "Bitness"
         bit8 = 1,
@@ -22,30 +24,37 @@
 
         // ARM
         Arm = 1 << 4,
-        Arm32 = (1 << 4) | bit32,
-        Arm64 = (1 << 4) | bit64,
+        Arm32 = Arm | bit32,
+        Arm64 = Arm | bit64,
 
         // Intel / AMD
         Intel = 1 << 5,
         x86 = Intel | bit32,
         x64 = Intel | bit64,
+
+        // x86 on ARM64
+        x86a64 = (x86 << 8) | Arm64
     }
 
     public static class Architectures
     {
         public static int GetBitnessInt(this Architecture arch)
         {
-            int n = (byte)arch & 0xFF;
+            var n = (byte)(arch & Architecture.bit512);
             // 2^(n + 2)
             return 1 << (n + 2);
         }
 
         public static Architecture GetBitness(this Architecture arch)
         {
-            int n = (byte)arch & 0xFF;
+            var n = (uint)arch & 0xFFu;
             return (Architecture)n;
         }
 
         public static Architecture Reduce(this Architecture arch) => arch & Architecture.Neutral;
+
+        public static Architecture Target(this Architecture arch) => (Architecture)((uint)arch & 0xFFu);
+
+        public static Architecture Emulated(this Architecture arch) => (Architecture)((uint)arch >> 8);
     }
 }
