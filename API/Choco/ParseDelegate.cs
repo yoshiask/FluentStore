b1394b132d3909ccf0a2e92b2869311a7846ceca
Joshua "Yoshi" Askharoun <jjask7@gmail.com>
2025-03-17 23:15:25 -0500

    Move parse delegate to own file

---
+++ API/Choco/ParseDelegate.cs
@@ -0,0 +1,6 @@
+﻿namespace Chocolatey;
+
+/// <summary>
+/// A method that parses a <see langword="string"/> into the specified <typeparamref name="T"/>.
+/// </summary>
+public delegate T Parse<T>(string str);
