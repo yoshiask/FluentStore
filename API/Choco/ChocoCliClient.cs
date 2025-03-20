d16d73ce35e361de4ee049f831ab9f9939131d18
Joshua "Yoshi" Askharoun <jjask7@gmail.com>
2025-03-20 00:02:30 -0500

    Add argument builder
    (cherry picked from commit 15e9888267baa2e14e1cdf1fd3566254734de66b)

--- API/Choco/ChocoCliClient.cs
+++ API/Choco/ChocoCliClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Chocolatey.Models;
@@ -11,8 +12,6 @@ namespace Chocolatey;
 public class ChocoCliClient : IChocoPackageService
 {
     private const string CHOCO_EXE = "choco";
-    private const string CHOCO_PARAM_YES = "-y";
-    private const string CHOCO_PARAM_LIMITOUTPUT = "--limit-output";
 
     private static readonly Regex _rxProgress = new(@"Progress: Downloading (?<id>[\w.\-_]+) (?<ver>\d+(\.\d+){0,3}(-[\w\d]+)?)\.\.\. (?<prog>\d{1,3})%", RegexOptions.Compiled);
 
@@ -20,17 +19,22 @@ public class ChocoCliClient : IChocoPackageService
     
     public async Task<bool> InstallAsync(string id, NuGetVersion? version = null, IProgress<PackageProgress>? progress = null)
     {
-        List<string> args = ["install", id, CHOCO_PARAM_YES, CHOCO_PARAM_LIMITOUTPUT];
-        
-        if (version is not null)
-            args.Add($"--version=\"'{version}'\"");
-        
-        if (NoOp)
-            args.Add("--noop");
+        List<string> args = ["install", id];
+
+        ChocoArgumentsBuilder argBuilder = new()
+        {
+            Version = version,
+            Yes = true,
+            LimitOutput = true,
+            NoOp = NoOp
+        };
+
+        argBuilder.Build(args);
         
         var result = await Cli.Wrap(CHOCO_EXE)
             .WithArguments(args)
             .WithStandardOutputPipe(PipeTarget.ToDelegate(HandleStdOut))
+            .WithValidation(CommandResultValidation.None)
             .ExecuteAsync();
         
         return result.ExitCode is 0;
@@ -53,7 +57,40 @@ public class ChocoCliClient : IChocoPackageService
 
     public async Task<bool> UninstallAsync(string id, NuGetVersion? version = null, IProgress<PackageProgress>? progress = null)
     {
-        throw new NotImplementedException();
+        List<string> args = ["uninstall", id];
+
+        ChocoArgumentsBuilder argBuilder = new()
+        {
+            Version = version,
+            Yes = true,
+            LimitOutput = true,
+            NoOp = NoOp
+        };
+
+        argBuilder.Build(args);
+
+        var result = await Cli.Wrap(CHOCO_EXE)
+            .WithArguments(args)
+            .WithStandardOutputPipe(PipeTarget.ToDelegate(HandleStdOut))
+            .WithValidation(CommandResultValidation.None)
+            .ExecuteAsync();
+
+        return result.ExitCode is 0;
+
+        void HandleStdOut(string line)
+        {
+            if (progress is null)
+                return;
+
+            var match = _rxProgress.Match(line);
+            if (!match.Success)
+                return;
+
+            var id = match.Groups["id"].Value;
+            var version = NuGetVersion.Parse(match.Groups["ver"].Value);
+            var percentage = int.Parse(match.Groups["prog"].Value);
+            progress.Report(new(id, version, percentage));
+        }
     }
 
     public async Task<bool> UpgradeAllAsync(IProgress<PackageProgress>? progress = null)
@@ -64,5 +101,31 @@ public class ChocoCliClient : IChocoPackageService
     public async Task<bool> UpgradeAsync(string id, IProgress<PackageProgress>? progress = null)
     {
         throw new NotImplementedException();
+    }
+
+    private class ChocoArgumentsBuilder
+    {
+        public NuGetVersion? Version { get; set; }
+        public bool Yes { get; set; }
+        public bool LimitOutput { get; set; }
+        public bool NoOp { get; set; }
+
+        public void Build(List<string> args) => args.AddRange(Build());
+
+        [Pure]
+        public IEnumerable<string> Build()
+        {
+            if (Version is not null)
+                yield return $"--version=\"'{Version}'\"";
+
+            if (Yes)
+                yield return "-y";
+
+            if (LimitOutput)
+                yield return "--limit-output";
+
+            if (NoOp)
+                yield return "--noop";
+        }
     }
 }
