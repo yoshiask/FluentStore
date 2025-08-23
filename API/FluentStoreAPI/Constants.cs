4d743ec415fbd2c6afec95eb923335588a6f56bb
Yoshi Askharoun <jjask7@gmail.com>
2025-08-23 13:56:11 -0500

    Update Supabase URL

--- API/FluentStoreAPI/Constants.cs
+++ API/FluentStoreAPI/Constants.cs
@@ -5,8 +5,8 @@ namespace FluentStoreAPI;
 
 public static class Constants
 {
-    public const string SUPABASE_URL = "https://nybfzmladevvhqcnkhnk.supabase.co";
-    public const string SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55YmZ6bWxhZGV2dmhxY25raG5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE2NTQ2MjUsImV4cCI6MjA1NzIzMDYyNX0.u0F5QLPdtsiWmjCnXEI-_wzP-WjDmo176zSMoK3Yl98";
+    public const string SUPABASE_URL = "https://obzzzkffllguthirhhku.supabase.co";
+    public const string SUPABASE_KEY = "sb_publishable_I_uZ1ypjQOujpYbxf8T3Rw_dS1p1SN_";
 
     public static IReadOnlyDictionary<FailureHint.Reason, string> GotrueReasons { get; } = new Dictionary<FailureHint.Reason, string>
     {
